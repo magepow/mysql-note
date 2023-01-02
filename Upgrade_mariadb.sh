01b16a4a6c8f30a1233953c5fe1d43c2cbfe16f8
Alex Dong <team.magiccart@gmail.com>
2023-01-02 11:04:31 +0700

    Update Upgrade_mariadb.sh

--- Upgrade_mariadb.sh
+++ Upgrade_mariadb.sh
@@ -1,1 +1,2 @@
-* [Started](https://wiki.centos-webpanel.com/mariadb-upgrade-from-10-2-to-10-4)
+* [Upgrade](https://mariadb.com/kb/en/upgrading)
+* [Upgrade 10.2 to 10.4](https://wiki.centos-webpanel.com/mariadb-upgrade-from-10-2-to-10-4)

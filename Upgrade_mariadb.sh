72471448af061aae7597ad60c07fc763467a91ea
magiccart <team.magiccart@gmail.com>
2023-12-19 18:14:18 +0700

    Update Upgrade_mariadb.sh

--- Upgrade_mariadb.sh
+++ Upgrade_mariadb.sh
@@ -1,2 +1,3 @@
 * [Upgrade](https://mariadb.com/kb/en/upgrading)
 * [Upgrade 10.2 to 10.4](https://wiki.centos-webpanel.com/mariadb-upgrade-from-10-2-to-10-4)
+* [Upgrade 10.4 to 10.6](https://mariadb.com/docs/server/service-management/upgrades/community-server/release-series-cs10-6/)
